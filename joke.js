25650a3f57caa3cd5f33156e3e51d9acee977fec
taylormathieu <taylor.mathieu@edu.sait.ca>
2025-03-20 13:49:01 -0600

    update joke.js
    adding the joke setup and delivery for the rest of the categories

--- joke.js
+++ joke.js
@@ -32,5 +32,67 @@ misc.addEventListener("click", function(){
    
 
 }); 
-
 })
+
+
+programming.addEventListener("click", function(){
+    console.log("programming");
+    fetch("https://v2.jokeapi.dev/joke/Programming?safe-mode")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        if(data.type == "single"){
+            joke.textContent = data.joke
+        }
+        else{
+            joke.textContent = data.setup + " " + data.delivery;
+        }
+    });
+})
+
+spooky.addEventListener("click", function(){
+    console.log("spooky");
+    fetch("https://v2.jokeapi.dev/joke/Spooky?safe-mode")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        if(data.type == "single"){
+            joke.textContent = data.joke
+        }
+        else{
+            joke.textContent = data.setup + " " + data.delivery;
+        }
+    });
+})
+
+christmas.addEventListener("click", function(){
+    console.log("christmas");
+    fetch("https://v2.jokeapi.dev/joke/Christmas?safe-mode")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        if(data.type == "single"){
+            joke.textContent = data.joke
+        }
+        else{
+            joke.textContent = data.setup + " " + data.delivery;
+        }
+    });
+})
+
+pun.addEventListener("click", function(){
+    console.log("pun");
+    fetch("https://v2.jokeapi.dev/joke/Pun?safe-mode")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        if(data.type == "single"){
+            joke.textContent = data.joke
+        }
+        else{
+            joke.textContent = data.setup + " " + data.delivery;
+        }
+    });
+})
+
+
